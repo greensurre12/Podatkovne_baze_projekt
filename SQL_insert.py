bd5f8a43c71979d8e37410cbb94689f8aef28caa
PavesicL <Pavesic.luka@gmail.com>
2017-05-06 20:24:02 +0200

    Koda, s katero lahko dodajas podatke v bazo.
    Trenutno so samo kraji, uporabnike lahko dodava na isti način.

---
+++ SQL_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*
+"""S tem sem vstavil kraje v tabelo kraji v bazi. Zakomentirana vrstica daje nek error."""
+
+from bottle import *
+import auth_public as auth
+import psycopg2, psycopg2.extensions, psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+
+
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
+conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+
+with open ("Seznam_krajev.txt", "r") as datoteka:
+    seznam_krajev = [i.strip()[5:] for i in datoteka]
+
+    
+#cur.executemany("""INSERT INTO kraj (ime) VALUES (%s)""", seznam_krajev) #TO NE DELA ZARAD NEKIH CUDNIH RAZLOGOV
+
+for i in seznam_krajev:
+    cur.execute("""INSERT INTO kraj (ime) VALUES ('{}')""".format(str(i)))
+
+run()
+
