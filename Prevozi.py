02fb5839077b11c6b9a9760fd88906244fb0099f
PavesicL <Pavesic.luka@gmail.com>
2017-05-19 14:23:13 +0200

    Neki sm narobe kliknu. Se vedno velik stvari, glej description
    -preimenoval glavni file iz Routes v Prevozi zaradi jasnosti
    -popravil template/redirect problem (razlozeno v komentarju na zacetku)
    -dodal piskotke tako da delajo in dodal avtomaticni login
    -preimenoval vse spremenljivke tipa name ipd. v uporabnik zaradi
    jasnosti
    -glavna stran ime zdaj ime <uporabnik>, naj bodo vsi nadaljni moduli en
    nivo nizje npr. modul objavi je zdaj na /<uporabnik>/objavi

--- Prevozi.py
+++ Prevozi.py
@@ -6,24 +6,48 @@ import hashlib # računanje MD5 kriptografski hash za gesla
 import auth_public as auth
 import psycopg2, psycopg2.extensions, psycopg2.extras
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
-import funkcije 
+from funkcije import * 
 import sqlite3
 from datetime import datetime
 ######################################################################
 
-conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password) #GLOBALNO
-conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)                          #
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password) #
+conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)                          #GLOBALNO
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)                                      #
 
 secret = "to je signature key za cookije, zato naj bo nek nakjlučni niz aosfh1309uhn0f1j1f9hj"
+######################################################################
+"""PREBERI TO
+-cookies: cookie ima ime account, v njem je shranjeno uporabnisko ime, hashano ker ga lahko kdorkoli bere
+-redirect: ce hoces na istem naslovu pokazat nekaj drugega, uporabis return(template), ce pa hoces uporabnika
+ poslat na drugo stran, uporabis redirect. Argumente v redirect shranis v url, ki ga posljes ali pa v cookie.
+-ZDI SE MI da je treba ob vsaki novi preusmeritvi cookie preveriti
+"""
+
 
 ######################################################################
 """PRVA STRAN"""
 
 @route("/")
 def prva_stran():
-    return template("prva_stran.html", napaka=False)
+    cookie = str(request.get_cookie('account', secret=secret))
+    cur.execute("SELECT 1 FROM uporabnik WHERE username=%s", [cookie])    
 
+    if cur.rowcount == 0:        
+        return template("prva_stran.html", napaka=False) #ce ze ma cookie ga avtomatsko logira
+    else:
+        return redirect('/{0}'.format(cookie))
+
+######################################################################
+"""GLAVNA STRAN"""
+
+@route("/<uporabnik>")
+def glavna_stran(uporabnik):
+    cookie = request.get_cookie('account', secret=secret)
+    if str(cookie) == uporabnik:
+        return template("glavna_stran.html", napaka=False, uporabnik=uporabnik)
+    else:
+        return redirect("/")
 
 ######################################################################
 """SIGNUP"""
@@ -36,8 +60,7 @@ def signup():
 @route('/signup', method= "POST")
 def signup():
     """Registrira novega uporabnika."""
-    
-        
+         
     uporabnisko_ime = bottle.request.forms.uporabnisko_ime 
     geslo = bottle.request.forms.geslo
     potrdi_geslo = bottle.request.forms.potrdi_geslo
@@ -60,9 +83,8 @@ def signup():
     else:
         geslo = funkcije.password_md5(geslo) #zakodira geslo za shranjevanje
         cur.execute("INSERT INTO uporabnik (id, username, geslo, telefon) VALUES (DEFAULT, %s, %s, %s)", (uporabnisko_ime, geslo, telefon))
-            
-        bottle.response.set_cookie("account", uporabnisko_ime, secret=secret) #cookie 
-        return template("glavna_stran.html", uporabnik = uporabnisko_ime, napaka=False) #na glavno stran
+        bottle.response.set_cookie("account", value=uporabnisko_ime, secret=secret, path='/') #cookie 
+        return redirect('/glavna/{0}'.format(uporabnisko_ime)) #na glavno stran
 
 ########################################################################################        
 """LOGIN"""
@@ -74,44 +96,42 @@ def login():
 
 @route('/login', method="POST")
 def login():
-    print("login1")
     
     uporabnisko_ime = bottle.request.forms.uporabnisko_ime 
     geslo = funkcije.password_md5(bottle.request.forms.geslo)
     cur.execute("SELECT 1 FROM uporabnik WHERE username=%s AND geslo=%s",[uporabnisko_ime, geslo])
 
     if cur.rowcount == 0: #ce SELECT ne najde nicesar -> ta kombinacija (username, password) ne obstaja
-        print("login2")
         return bottle.template("login.html", napaka="Napacno uporabnisko ime ali geslo!")
 
-    else:
-        print("login3")
-        bottle.response.set_cookie('account', uporabnisko_ime, secret=secret)
-        return template("glavna_stran.html", uporabnik = uporabnisko_ime, napaka=False)
-        #return redirect("/<username>")
+    else: #uspesen login
+        bottle.response.set_cookie('account', value=uporabnisko_ime, secret=secret, path='/')
+        return redirect('/{0}'.format(uporabnisko_ime))
+
+
 ########################################################################################     
 """LOGOUT"""
 
 @bottle.get('/logout')
 def logout():
     """Pobriši cookie in preusmeri na login."""
-    bottle.response.delete_cookie('account')
-    return template("prva_stran.html",napaka=False)
+    bottle.response.delete_cookie('account',secret=secret)
+    return redirect('/')
 
 ########################################################################################
 """OBJAVA PREVOZA"""
 
-@route('/objavi/<name>')
-def objavi(name):
-    cur.execute("SELECT 1 FROM uporabnik WHERE username=%s", [name])
+@route('/<uporabnik>/objavi')
+def objavi(uporabnik):
+    cur.execute("SELECT 1 FROM uporabnik WHERE username=%s", [uporabnik])
 
     if cur.rowcount == 0: #ce uporabnika ni v bazi, npr če naroke napišeš /objavi/neobstoječ_uporabnik , to ne sme!!
         return template("prva_stran.html", napaka = "Žal ta uporabnik ne obstaja!")
     else:
-        return template("objavi.html",uporabnik = name,napaka=False, zdaj = datetime.now().strftime('%Y-%m-%dT%H:%M')) #tisti koledar nebo dovolil vnosa manjsega od zdaj
+        return template("objavi.html",uporabnik = uporabnik,napaka=False, zdaj = datetime.now().strftime('%Y-%m-%dT%H:%M')) #tisti koledar nebo dovolil vnosa manjsega od zdaj
     
-@route('/objavi/<name>', method = "POST")
-def objavi_post(name):
+@route('/<uporabnik>/objavi', method = "POST")
+def objavi_post(uporabnik):
     ''' /objavi/<name> : forma v html na glavni strani sprejme /objavi/upobraniško_ime, zato da ga lahko upiševa v bazo.
     nekako morava zaščititi to, da lahko dostopiš do wwww.spletna.com/objavi/, le če si loginan, drugače te mora zavrniti'''
     zacetek = bottle.request.forms.zacetek
@@ -119,59 +139,34 @@ def objavi_post(name):
     konec = bottle.request.forms.konec
     koncni_kraj = bottle.request.forms.koncni_kraj
     prosta_mesta = bottle.request.forms.prosta_mesta
-    uporabnisko_ime = name
+    uporabnisko_ime = uporabnik
     print("objavi2")
 
     ##
     cur.execute("SELECT 1 FROM kraj WHERE ime=%s",[zacetni_kraj])
     if cur.rowcount == 0:
-        return template("objavi.html", napaka = "Zacetni kraj ne obstaja!", uporabnik = name, zdaj = datetime.now().strftime('%Y-%m-%dT%H:%M'))
+        return template("objavi.html", napaka = "Zacetni kraj ne obstaja!", uporabnik = uporabnik, zdaj = datetime.now().strftime('%Y-%m-%dT%H:%M'))
 
     cur.execute("SELECT 1 FROM kraj WHERE ime=%s",[koncni_kraj])
     if cur.rowcount == 0:
-        return template("objavi.html", napaka = "Koncni kraj ne obstaja!", uporabnik = name, zdaj = datetime.now().strftime('%Y-%m-%dT%H:%M'))
+        return template("objavi.html", napaka = "Koncni kraj ne obstaja!", uporabnik = uporabnik, zdaj = datetime.now().strftime('%Y-%m-%dT%H:%M'))
 
     else:
-        id = cur.execute("SELECT id FROM uporabnik WHERE username=%s", [name]) #poisce id uporabnika
+        id = cur.execute("SELECT id FROM uporabnik WHERE username=%s", [uporabnik]) #poisce id uporabnika
         cur.execute("INSERT INTO prevoz (id,objavil,  zacetek,zacetni_kraj,konec,koncni_kraj,prosta_mesta) VALUES (DEFAULT,%s,%s,%s,%s,%s,%s)",
-                                        (name,zacetek,zacetni_kraj,konec,koncni_kraj,prosta_mesta)) 
-        return template("glavna_stran.html",uporabnik=name, napaka= "Uspesno si objavil prevoz!")
-        
-    ##
+                                        (uporabnik,zacetek,zacetni_kraj,konec,koncni_kraj,prosta_mesta)) 
+        return template("glavna_stran.html",uporabnik=uporabnik, napaka= "Uspesno si objavil prevoz!")
 
+########################################################################################    
+"""SEARCH"""
+@route('/<uporabnik>/isci')
+def isci(uporabnik):
+    uporabnik = request.get_cookie('account', secret=secret)
+    if uporabnik == None
+        return redirect("/")
     
-    #TO NEKI NE DELA, KAJ CE BI NAREDILA KOT JE ZE PRI OMEJITVAH ZA USERNAME/GESLO
-    #error = funkcije.validate_form(request.forms, [ "zacetni_kraj", "koncni_kraj", "zacetek","konec"]) #preveri ali je kaj prazno validate_form je v funkcije.py 
-    #if error is []:
-    #    napaka = False
-    #    print('ni napake')
-    #else:
-    #    print('je napaka')
-    #    napaka = "Prazno : " + str(error)
-    #    return template("objavi.html", napaka = napaka, uporabnik = name)
-    # 
-    #
-    #    
-    #cur.execute("SELECT 1 FROM kraj WHERE ime = %s", [zacetni_kraj]) #zaenkrat tukaj preveriva ali je kraj OK, kasneje z Javascript že na sami strani!"
-    #if cur.rowcount == 0: 
-    #    return template("objavi.html", uporabnik = name,napaka="Kraj '%s' ne obstaja! Preveri ali si ga pravilno zapisal \n primer: Bohinjska Bela, Bled"% (zacetni_kraj))
-    # 
-    #cur.execute("SELECT 1 FROM kraj WHERE ime = %s", [koncni_kraj]) #zaenkrat tukaj preveriva ali je kraj OK!"
-    #if cur.rowcount == 0: 
-    #    return template("objavi.html", uporabnik = name, napaka="Kraj '%s' ne obstaja! Preveri ali si ga pravilno zapisal \n primer: Bohinjska Bela, Bled" %(koncni_kraj))
-    #        
-    #
-    #
-    #
-    #
-    #id = cur.execute("SELECT id FROM uporabnik WHERE username=%s", [name]) #poisce id uporabnika
-    #cur.execute("INSERT INTO prevoz (id,objavil,zacetek,zacetni_kraj,konec,koncni_kraj,prosta_mesta) VALUES (DEFAULT, %s, %s, %s,%s,%s,%s)", 
-    #            (id, zacetek, zacetni_kraj, konec, koncni_kraj,prosta_mesta))
-    # 
-    #return template("glavna_stran.html",uporabnik=name, napaka= "Uspesno si objavil prevoz!")
-    
-    
-    
+    else:
+        return template("iskanje.html")
     
 
 run()
